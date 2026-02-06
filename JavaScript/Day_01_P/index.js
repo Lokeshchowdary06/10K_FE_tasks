cc0fba15c93ae95b01fc62cfb1c3c6e3912ea672
unknown <lokeshchowdary569@gmail.com>
2026-02-06 11:41:12 +0530

    Day_01_Practice

---
+++ JavaScript/Day_01_P/index.js
@@ -0,0 +1,11 @@
+//Even or Odd Number
+var a= prompt("Enter a Number to Check Even or Odd: ");
+console.log(a%2==0?"Even Number":"Odd Number");
+
+//Adult and Minor
+var age= prompt("Enter Age: ");
+console.log(age>18?"Adult":"Minor");
+
+//Grade Classification
+var grade= prompt("Enter Student Grade:")
+console.log(grade>=90?"A Grade":grade>=75?"B Grade":grade>=60?"C Grade":grade>=45?"D Garde":"Fail");
