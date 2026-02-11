326f90d90d47954bbef069090f2134067e56a4bd
unknown <lokeshchowdary569@gmail.com>
2026-02-11 10:30:35 +0530

    Js Functions-Task_1

---
+++ JavaScript/Day_02_P/index.js
@@ -0,0 +1,44 @@
+/**
+ * Java Script Functions_1 Task
+ */
+//Task-1
+function greet()
+{
+    console.log("Hello!");   
+}
+greet();
+//Task-2
+function showMessages()
+{
+    console.log("This is a message!");
+}
+showMessages();
+//Task-3
+function logInfo()
+{
+    console.log("Logging Information..");
+}
+logInfo();
+//task-4
+function startGame()
+{
+    console.log("Game Started!");
+    endGame();
+}
+function endGame()
+{
+    console.log("Game Over!");
+    
+}
+startGame();
+//Task-5
+function beginTask()
+{
+    console.log("Task Started");
+    finishTask();
+}
+function finishTask()
+{
+    console.log("Task Finished");
+}
+beginTask();
